4f509b21f84357cec81484d4217320fd8faa1e8b
Armon Toubman <armon@armontoubman.com>
2020-07-21 15:04:55 +0200

    Added quotes for bash.

--- src/config.lisp
+++ src/config.lisp
@@ -23,7 +23,7 @@
   `(:application-root ,(asdf:component-pathname (asdf:find-system :recepten))))
 
 ;; start with: APP_ENV=development clackup --address 0.0.0.0 app.lisp
-;; or with: echo "export APP_ENV=development" >> ~/.bashrc
+;; or with: echo "export APP_ENV=\"development\"" >> ~/.bashrc
 ;;          (ql:quickload :recepten) (recepten:start :port 5000)
 (defconfig |development|
   `(:debug T
